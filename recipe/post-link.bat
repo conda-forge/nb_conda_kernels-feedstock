6280b5446ff241677b2d42662fde909b8cc68c25
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-21 00:19:02 -0400

    try against conda-forge vs2015

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,3 +1,1 @@
-(
-  "%PREFIX%\python.exe" -d -m nb_conda_kernels.install --enable --prefix="%PREFIX%"
-) >>"%PREFIX%\.messages.txt" 2>&1
+"%PREFIX%\python.exe" -d -m nb_conda_kernels.install --enable --prefix="%PREFIX%"
