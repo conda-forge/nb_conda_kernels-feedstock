eab170eccfe87f21087b1edbc02946c78a4191db
Filipe Fernandes <ocefpaf@gmail.com>
2018-07-07 10:07:57 -0300

    fix scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
-mkdir Scripts
-copy %SRC_DIR%\conda-recipe\post-link.bat Scripts\.nb_conda_kernels-post-link.bat
-copy %SRC_DIR%\conda-recipe\pre-unlink.bat Scripts\.nb_conda_kernels-pre-unlink.bat
+%PYTHON% -m pip install --no-deps --ignore-installed .
+if errorlevel 1 exit 1
+
+set SCRIPTS="%PREFIX%\Scripts"
+if not exist %SCRIPTS% mkdir %SCRIPTS%
+if errorlevel 1 exit 1
+
+copy %SRC_DIR%\conda-recipe\post-link.bat %SCRIPTS%\.nb_conda_kernels-post-link.bat || exit 1
+copy %SRC_DIR%\conda-recipe\pre-unlink.bat %SCRIPTS%\.nb_conda_kernels-pre-unlink.bat || exit 1
